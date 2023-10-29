e42365f7ee004d4fef6ee683a28abeb42e36b342
jiwon317 <chlghqlt12@naver.com>
2023-10-29 23:12:05 +0900

    Ex4_8

--- src/ch04/Ex4_7.java
+++ src/ch04/Ex4_7.java
@@ -6,8 +6,10 @@ public class Ex4_7 {
 		
 		// {}안의 내용을 20번 반복한다.
 		// Quiz. 1~10 사이의 난수를 20개 출력하시오.
+		// Quiz2. -5~5사이의 난수 20개출력
+		// -5-4-3-2-1012345 11개
 		for (int i = 1; i <= 20; i++) {
-			num = (int) (Math.random() * 10); // 0<=x<10
+			num = (int) (Math.random() * 11) -5; // 0<=x<10
 			System.out.println(num);
 		}		
 	}
