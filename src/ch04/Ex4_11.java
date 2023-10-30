5b972edbf260bd109ea694b8e92612aa7ea99852
jiwon317 <chlghqlt12@naver.com>
2023-10-31 00:47:37 +0900

    Ex4_11_2

--- src/ch04/Ex4_11.java
+++ src/ch04/Ex4_11.java
@@ -3,11 +3,18 @@ package ch04;
 public class Ex4_11 {
 	public static void main(String args[]) {
 		
-		for(int i = 2; i <= 9; i++) {
-			for(int j = 1; j <= 9; j++) {
-				System.out.println(i+"*"+j+"="+(i*j));
+		for(int i = 1; i <= 5; i++) {
+//			for(int j = 1; j <= 9; j++) {
+//				System.out.println(i+"*"+j+"="+(i*j));
+//			}
+//			System.out.println();
+		for(int j=1;j<=i;j++) {
+			System.out.print("*");
+//			System.out.println("**********");
+		
 			}
-			System.out.println();
-		}
+			System.out.println();//줄바꿈
+		}	
 	}
-}
+}	
+// i가 y축 j가 x축
