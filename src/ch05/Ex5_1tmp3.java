25bef3f64c144e4fa6ba7160a6fb5151b99c7c48
jiwon317 <chlghqlt12@naver.com>
2023-11-03 16:11:57 +0900

    Ex5_1tmp3

---
+++ src/ch05/Ex5_1tmp3.java
@@ -0,0 +1,13 @@
+package ch05;
+
+import java.util.Arrays;
+public class Ex5_1tmp3 {
+	public static void main(String args[]) {
+		int[] iArr = {100, 95, 80, 70,60};
+		for(int i = 0; i < iArr.length; i++) {
+			System.out.println(iArr[i]);
+		}
+		
+		System.out.println(Arrays.toString(iArr));
+	}
+}
