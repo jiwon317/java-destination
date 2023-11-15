249227aa1d306a633b78832902fa388769bcb2fe
jiwon317 <chlghqlt12@naver.com>
2023-11-15 23:32:48 +0900

    Ex5_2tmp3

--- src/ch05/Ex5_2tmp.java
+++ src/ch05/Ex5_2tmp.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 
 public class Ex5_2tmp {
 	public static void main(String args[]) {
-		int[] arr = {0,1,2,3,4}; // 1차원 배열 arr
+		int[] arr = {3,4,1,0,2}; // 1차원 배열 arr
 		int[][] arr2D = {{11,12},{21,22}}; // 2차원 배열
-		
+		System.out.println(Arrays.toString(arr));
+		Arrays.sort(arr); // 배열 arr을 오름차순으로 정렬
+		System.out.println(Arrays.toString(arr));
 		System.out.println(Arrays.toString(arr)); 
 		System.out.println(Arrays.deepToString(arr2D));
 		
@@ -15,5 +17,10 @@ public class Ex5_2tmp {
 		
 		System.out.println(Arrays.deepEquals(str2D,str2D2));
 		
+		int[] arr2 = Arrays.copyOf(arr, 3);
+		System.out.println(Arrays.toString(arr2)); 
+		
+		int[] arr3 = Arrays.copyOfRange(arr, 2, 4);
+		System.out.println(Arrays.toString(arr3)); 
 	}
 }
