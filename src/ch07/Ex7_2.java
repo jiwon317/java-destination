a4c67d7487b389fd82dedbe700599f7e5345467d
jiwon317 <chlghqlt12@naver.com>
2023-11-17 18:27:29 +0900

    Ex7_2

--- src/ch07/Ex7_2.java
+++ src/ch07/Ex7_2.java
@@ -12,7 +12,7 @@ class Child extends Parent {
 	int x = 20; // this.x
 	
 	void method() {
-		System.out.println("x=" + x);
+		System.out.println("x=" +x);
 		System.out.println("this.x=" + this.x);
 		System.out.println("super.x=" + super.x);
 	}
