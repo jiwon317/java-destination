d27fac5ea84a0349cac5bd1359bd3bb955fad763
jiwon317 <chlghqlt12@naver.com>
2023-11-23 01:36:31 +0900

    Ex8_8

--- src/ch08/Ex8_7.java
+++ src/ch08/Ex8_7.java
@@ -2,6 +2,6 @@ package ch08;
 
 public class Ex8_7 {
 	public static void main(String[] args) {
-//		throw new Exception();					 // Exception을 고의로 발생시킴
+//		throw new Exception();					 // Exception을 고의로 발생시킴, 실행 안됨
 	}
 }
