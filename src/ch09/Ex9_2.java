2beb3afec998869100b0285825498023990a2238
jiwon317 <chlghqlt12@naver.com>
2023-11-27 23:58:40 +0900

    Ex9_2

---
+++ src/ch09/Ex9_2.java
@@ -0,0 +1,28 @@
+package ch09;
+
+class Person {
+	long id;
+	
+	public boolean equals(Object obj) {
+		if(obj instanceof Person)
+			return id == ((Person)obj).id;  // obj가 Object 타입이므로 id값을 참조하기 위해서는 Person 타입으로 형변환 필요
+		else
+			return false;						// 타입이 Person이 아니면 값을 비교할 필요도 없다.
+	}
+	
+	Person(long id) {
+		this.id = id;
+	}
+}
+
+public class Ex9_2 {
+	public static void main(String args[]) {
+		Person p1 = new Person(8011081111222L);
+		Person p2 = new Person(8011081111222L);
+		
+		if(p1.equals(p2))
+			System.out.println("p1과 p2는 같은 사람입니다.");
+		else
+			System.out.println("p1과 p2는 다른 사람입니다.");
+	}
+}
