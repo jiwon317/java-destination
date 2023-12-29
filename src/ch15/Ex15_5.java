305963b65011aaf98fe824ca57be5ecd89fd0a65
jiwon317 <chlghqlt12@naver.com>
2023-12-29 23:47:05 +0900

    Ex15_6

--- src/ch15/Ex15_5.java
+++ src/ch15/Ex15_5.java
@@ -5,17 +5,18 @@ import java.io.*;
 class FileCopy {
 	public static void main(String args[]) {
 		try {
-			FileInputStream  fis = new FileInputStream(args[0]);
+			FileInputStream fis = new FileInputStream(args[0]);
 			FileOutputStream fos = new FileOutputStream(args[1]);
-			
-			int data=0;
-			while((data=fis.read())!=-1)
-				fos.write(data);;		// void write(int b)
-				
+
+			int data = 0;
+			while ((data = fis.read()) != -1)
+				fos.write(data);
+			; // void write(int b)
+
 			fis.close();
 			fos.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}	
+	}
 }
